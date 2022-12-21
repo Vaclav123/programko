7256b71ea469cfb08e19c6143d099cef5d2765de
Vaclav123 <121161331+Vaclav123@users.noreply.github.com>
2022-12-21 10:32:41 +0100

    A
    B

---
+++ BANKA.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int (cislo);
+cout << "BANKA JANKA \n";
+cout << "\n\n";
+cout << "1) vklad  2) vyber  3) zmena jazyka \n";
+cout << "4) napoveda  5) informace";
+cout << "\n\n";
+
+cin >> cislo;
+
+switch (cislo) {
+  case 1:
+    cout << "vklad";
+    break;
+  case 2:
+    cout << "vyber";
+    break;
+  case 3:
+    cout << "zmena jazyka";
+    break;
+  case 4:
+    cout << "napoveda";
+    break;
+  case 5:
+    cout << "informace";
+    break;
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+}
